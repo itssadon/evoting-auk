f9c31b4b7279ca9c17bfcbdf94c7e56113d26624
Sadiq Hassan <abubakarhassan59@gmail.com>
2017-04-04 18:28:53 +0100

    Client components, guards, services

---
+++ client/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+    constructor(
+        private router: Router,
+        private authService: AuthService,
+        private flashMessage: FlashMessagesService
+    ) { }
+
+    ngOnInit() {
+    }
+
+    onLogoutClick() {
+        this.authService.logout();
+        this.flashMessage.show('You are now logged out', {
+            cssClass: 'alert-success',
+            timeout: 3000
+        });
+        this.router.navigate(['/login']);
+        return false;
+    }
+
+}
