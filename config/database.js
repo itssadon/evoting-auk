b6e8ae74f3975d0619c770cc321e09d8c2a8ad43
Sadiq Hassan <abubakarhassan59@gmail.com>
2017-04-25 21:46:44 +0100

    Master commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://evoting:evotingAdmin@ds115671.mlab.com:15671/evoting-atbu', //'mongodb://localhost:27017/evoting-atbu',
+    database: 'mongodb://localhost:27017/evoting-atbu',
     secret: 'yoursecret'
 }
