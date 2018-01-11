122f0809f50f1de2e13f64a3ce507f3f95fc8769
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-01-11 18:04:33 +0100

    Modified database connection

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
-/*
 module.exports = {
     database: 'mongodb://evoting:evotingAdmin@ds247347.mlab.com:47347/evoting-fptb',
     secret: 'yoursecret'
 }
-*/
 
+/*
 module.exports = {
     database: 'mongodb://localhost:27017/evoting-fptb',
     secret: 'yoursecret'
 }
+*/
