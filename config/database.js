a84d7da3afaa0b7c9ff97898203976443ef3158f
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-08-09 18:32:34 +0100

    Switched to heroku mongo database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
-    database: 'mongodb://evoting:evotingAdmin1@ds247587.mlab.com:47587/evoting-auk',
+    database: 'mongodb://heroku_1xcc06pl:f8qdm8k22kdhdk1sullld00oqj@ds117422.mlab.com:17422/heroku_1xcc06pl',
+    // 'mongodb://evoting:evotingAdmin1@ds247587.mlab.com:47587/evoting-auk',
     secret: 'yoursecret'
 }
 
